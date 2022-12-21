5e737dd1509cfcbdbc59b3983abeee2678176fe7
Gloria Dyah Pramesti <91613088+gloriadya@users.noreply.github.com>
2022-12-21 19:19:05 +0700

    Add files via upload

---
+++ proses-edit.php
@@ -0,0 +1,27 @@
+<?php
+include("config.php");
+
+if (isset($_POST['simpan'])) {
+    $id = $_POST['id'];
+    $nama = $_POST['nama'];
+    $alamat = $_POST['alamat'];
+    $jk = $_POST['jenis_kelamin'];
+    $agama = $_POST['agama'];
+    $fotobaru = $_POST['foto'];
+
+    // Rename nama fotonya dengan menambahkan tanggal dan jam upload
+    $fotobaru = date('dmYHis').$foto;
+    // Set path folder tempat menyimpan fotonya
+    $path = "img/".$fotobaru;
+
+    $sql = "Update tambah_foto set nama='$nama', alamat='$alamat', jenis_kelamin='$jk', agama='$agama', sekolah_asal='$sekolah',  foto='$fotobaru' Where id=$id";
+    $query = mysqli_query($db, $sql);
+
+    if ($query) {
+        header('Location: list-siswa.php');
+    } else {
+        die("Gagal menyimpan perubahan...");
+    }
+} else {
+    die("Akses dilarang...");
+}
